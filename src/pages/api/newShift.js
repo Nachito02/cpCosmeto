d9c18a8007a690444813ddf8d586c4beffa14742
Nachito02 <arguellojuan08@gmail.com>
2023-05-18 14:58:57 -0300

    añadiendo panel para reservar los servicios

--- src/pages/api/newShift.js
+++ src/pages/api/newShift.js
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import Service from "./models/Categories"
 import Shifts from "./models/Shifts"
 export default function handler(req, res) {
 
